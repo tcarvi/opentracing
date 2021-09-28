c363807338f4dd9d01e03b2ed51c7b53e1b70045
TCARVI <tcarvi@me.com>
2021-09-27 21:08:16 -0300

    Update

--- src/main/java/com/tcarvi/opentracingquarkustemplate/rest/client/RestClientResource.java
+++ src/main/java/com/tcarvi/opentracingquarkustemplate/rest/client/RestClientResource.java
@@ -5,6 +5,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import io.vertx.core.json.JsonObject;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
@@ -12,44 +13,36 @@ import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import io.smallrye.mutiny.Uni;
 
-import io.micrometer.core.instrument.MeterRegistry;
-
 @Path("/restClient")
 public class RestClientResource implements RestClientResourceInterface {
-
-    private final MeterRegistry registry;
-
-    RestClientResource(MeterRegistry registry) {
-        this.registry = registry;
-    }
 
     @Inject
     RestClientService restClientService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String restClientResourceExecution(){
+    public JsonObject restClientResourceExecution(){
         return restClientService.exec();
     }
 
     @GET
     @Path("/name/{name}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getName(@PathParam String name) {
+    public JsonObject getName(@PathParam String name) {
         return restClientService.exec(name);
     }
 
     @GET
     @Path("/name-async/{name}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getNameAsync(@PathParam String name) {
+    public JsonObject getNameAsync(@PathParam String name) {
         return restClientService.exec(name);
     }
 
     @GET
     @Path("/name-uni/{name}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getNameMutiny(@PathParam String name) {
+    public JsonObject getNameMutiny(@PathParam String name) {
         return restClientService.exec(name);
     }
 
